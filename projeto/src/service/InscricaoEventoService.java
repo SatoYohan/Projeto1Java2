1b4de3e36ac37dc182ce9162db666920206d8163
Abner Yohan Sato <102881830+SatoYohan@users.noreply.github.com>
2025-02-17 21:23:21 -0300

    fix src/gui para os modulo de eventos

--- projeto/src/service/InscricaoEventoService.java
+++ projeto/src/service/InscricaoEventoService.java
@@ -13,55 +13,26 @@ import entities.InscricaoEvento;
 
 public class InscricaoEventoService {
 
-    public InscricaoEventoService() {
-
-    }
-    
-	public void cadastrar(InscricaoEvento inscricaoEvento) throws SQLException, IOException {
-
-		Connection conn = BancoDados.conectar();
-		new InscricaoEventoDAO(conn).cadastrar(inscricaoEvento);
-	}
-
-    public boolean verificarInscricaoExistente(int codigoParticipante, int codigoEvento) throws SQLException, IOException {
-        return new InscricaoEventoDAO(BancoDados.conectar()).verificarInscricaoExistente(codigoParticipante, codigoEvento);
-        
-    }
-	
-    public List<InscricaoEvento> buscarEventosPorParticipante(int codigoParticipante) throws SQLException, IOException {
-        
-    		Connection conn = null;
-    		if (conn == null || conn.isClosed()) {
-    			conn = BancoDados.conectar(); 
-    		}
-        
-            InscricaoEventoDAO inscricaoEventoDAO = new InscricaoEventoDAO(conn);
-            List<InscricaoEvento> inscricoes = inscricaoEventoDAO.buscarEventosPorParticipante(codigoParticipante);
-
-            List<Evento> eventos = new ArrayList<>();
-            for (InscricaoEvento inscricao : inscricoes) {
-                eventos.add(inscricao.getEvento());
-            }
-
-            return inscricoes;
-        
-    }
-	
-	public void confirmarPresenca(int codigoPessoa, int codigoEvento) throws SQLException, IOException {
-		
-		Connection conn = BancoDados.conectar();
-	    new InscricaoEventoDAO(conn).confirmarPresenca(codigoPessoa, codigoEvento);
-	}
-
-	public void cancelarInscricao(int codigoPessoa, int codigoEvento) throws SQLException, IOException {
-		Connection conn = BancoDados.conectar();
-	    new InscricaoEventoDAO(conn).cancelarInscricao(codigoPessoa, codigoEvento);
+	public InscricaoEventoService() {
 
 	}
-	
-	public String buscarStatusEvento(int codigoEvento) throws SQLException, IOException {
-		Connection conn = BancoDados.conectar();
-	    return new InscricaoEventoDAO(conn).buscarStatusEvento(codigoEvento);
+
+	public List<Evento> buscarEventosPorParticipante(int codigoParticipante) throws SQLException, IOException {
+
+		Connection conn = null;
+		if (conn == null || conn.isClosed()) {
+			conn = BancoDados.conectar();
+		}
+
+		InscricaoEventoDAO inscricaoEventoDAO = new InscricaoEventoDAO(conn);
+		List<InscricaoEvento> inscricoes = inscricaoEventoDAO.buscarEventosPorParticipante(codigoParticipante);
+
+		List<Evento> eventos = new ArrayList<>();
+		for (InscricaoEvento inscricao : inscricoes) {
+			eventos.add(inscricao.getEvento());
+		}
+
+		return eventos;
 
 	}
 }
